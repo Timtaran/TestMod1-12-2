73f464c2362dcc1ca7516025014292a984260f14
Timtaran <noEmail@timtaran.com>
2021-09-25 22:29:08 +0500

    0.0.4 update

--- src/main/java/com/example/examplemod/Blocks2.java
+++ src/main/java/com/example/examplemod/Blocks2.java
@@ -20,10 +20,12 @@ public class Blocks2 {
     @SubscribeEvent
     public static void block_break(final BlockEvent.BreakEvent event) {
         if (event.getWorld().getBlockState(event.getPos()).getBlock() instanceof BlockLog) {
-            ITextComponent text = new TextComponentString(TextFormatting.GOLD + "[\u0418\u043d\u0432\u0435\u043d\u0442\u0430\u0440\u044c]" + TextFormatting.RESET + " >> " + TextFormatting.GREEN + "\u0421\u043f\u0430\u0441\u0438\u0431\u043e \u0437\u0430 \u0434\u043e\u0431\u044b\u0447\u0443 \u0434\u0435\u0440\u0435\u0432\u0430.");
-            Block blockIn = event.getState().getBlock();
-            Item itemInHand = event.getPlayer().getHeldItem(event.getPlayer().getActiveHand()).getItem();
-            event.getPlayer().sendMessage(text);
+            if (Config.getBoolean("main", "invspk")) {
+                ITextComponent text = new TextComponentString(TextFormatting.GOLD + "[\u0418\u043d\u0432\u0435\u043d\u0442\u0430\u0440\u044c]" + TextFormatting.RESET + " >> " + TextFormatting.GREEN + "\u0421\u043f\u0430\u0441\u0438\u0431\u043e \u0437\u0430 \u0434\u043e\u0431\u044b\u0447\u0443 \u0434\u0435\u0440\u0435\u0432\u0430.");
+                Block blockIn = event.getState().getBlock();
+                Item itemInHand = event.getPlayer().getHeldItem(event.getPlayer().getActiveHand()).getItem();
+                event.getPlayer().sendMessage(text);
+            }
         }
     }
     @SubscribeEvent
@@ -36,11 +38,11 @@ public class Blocks2 {
                 //Minecraft.getMinecraft().player.sendChatMessage(TextFormatting.GOLD + "[TimTarAn]" + TextFormatting.RESET + " >> " + TextFormatting.GREEN + "\u0423\u0420\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410, \u0420\u0410\u0411\u041e\u0422\u0410\u0415\u0422, \u041d\u0410\u041a\u041e\u041d\u0415\u0426-\u0422\u041e!!!");
                 // TimTarAn lost connection: Недопустимые символы в чате
                 Minecraft.getMinecraft().player.sendMessage(text);
-                Config.writeConfig("main", "invspk", true);
+                Config.writeConfig("main", "invspk", false);
                 event.setCanceled(true);
             }
             else {
-                ITextComponent text = new TextComponentString(TextFormatting.GOLD + "[Инвентарь]" + TextFormatting.RESET + " >> " + TextFormatting.GREEN + "\u0420\u0435\u0436\u0438\u043c \u0448\u0438\u0437\u043e\u0444\u0440\u0435\u043d\u0438\u0438 \u0432\u043a\u043b\u044e\u0447\u0435\u043d!");
+                ITextComponent text = new TextComponentString(TextFormatting.GOLD + "[\u0418\u043d\u0432\u0435\u043d\u0442\u0430\u0440\u044c]" + TextFormatting.RESET + " >> " + TextFormatting.GREEN + "\u0420\u0435\u0436\u0438\u043c \u0448\u0438\u0437\u043e\u0444\u0440\u0435\u043d\u0438\u0438 \u0432\u043a\u043b\u044e\u0447\u0435\u043d!");
                 //Minecraft.getMinecraft().player.sendChatMessage(TextFormatting.GOLD + "[TimTarAn] >> \u0423\u0420\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410, \u0420\u0410\u0411\u041e\u0422\u0410\u0415\u0422, \u041d\u0410\u041a\u041e\u041d\u0415\u0426-\u0422\u041e!!!");
                 //Minecraft.getMinecraft().player.sendChatMessage(TextFormatting.GOLD + "[TimTarAn]" + TextFormatting.RESET + " >> " + TextFormatting.GREEN + "\u0423\u0420\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410\u0410, \u0420\u0410\u0411\u041e\u0422\u0410\u0415\u0422, \u041d\u0410\u041a\u041e\u041d\u0415\u0426-\u0422\u041e!!!");
                 // TimTarAn lost connection: Недопустимые символы в чате
