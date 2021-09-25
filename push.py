bcb843a15a946c17b23dc274c1191878cb05aa85
Timtaran <timtaranlivecpgyyd@gmail.com>
2021-09-25 13:25:13 +0500

    ghfdgkhj

--- push.py
+++ push.py
@@ -1,8 +1,9 @@
 import os
 #git remote set-url origin https://token@github.com/UserName/RepositoryName.git
 while True:
+    un=input("Name of update >>  ")
     os.system('git add .')
-    os.system(f'git commit -m "{input("Name of update >>  ")}"')
+    os.system(f'git commit -m "{un}"')
     os.system(f'git push -u --force origin master')
     a=input('Нажмите Enter для завершения работы программы или g и Enter для повторного запуска...\n')
     if a.lower() != "g":
