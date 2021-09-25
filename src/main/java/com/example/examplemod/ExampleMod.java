1edd34286231f0b11f1d64f423e5331dd1ceb593
Timtaran <timtaranlivecpgyyd@gmail.com>
2021-09-25 13:17:26 +0500

    0.0.2

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -12,7 +12,7 @@ public class ExampleMod
 {
     public static final String MODID = "dat";
     public static final String NAME = "DimucaTheDev and TimTarAn Test Mod";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     private static Logger logger;
 
